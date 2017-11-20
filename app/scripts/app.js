2e6a29c3ad98824b16b4201a6e05e61b3b0859c2
Kriz Anthony Mayo <kmayo@kmayo.local>
2017-11-19 19:08:22 -0500

    Update foreach-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,9 +64,14 @@ Instructions:
      */
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
+      var sequence = Promise.resolve();
       response.results.forEach(function(url) {
-        getJSON(url).then(createPlanetThumb);
+        sequence = sequence.then(function() {
+          return getJSON(url);
+        }).then(createPlanetThumb);
       });
+    }).catch(function(error) {
+      console.log(error);
     });
   });
 })(document);
